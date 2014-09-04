c2ecc9059db0a9e786ba806a1e94d39bcfcf7334
Vincent Arrogancia <varrogancia@gmail.com>
2014-09-03 21:16:12 -0700

    Added style sheets. Functional

--- main.py
+++ main.py
@@ -36,9 +36,15 @@ class Handler(webapp2.RequestHandler):
 	def render(self, template, **kw):
 		self.write(self.render_str(template, **kw))
 
+class Art(db.Model):
+	title = db.StringProperty(required = True)
+	art = db.TextProperty(required = True)
+	created = db.DateTimeProperty(auto_now_add = True)
+
 class MainPage(Handler):
 	def render_front(self, title="", art="", error=""):
-		self.render("front.html", title=title, art=art, error=error)
+		arts = db.GqlQuery("SELECT * FROM Art " "ORDER BY created DESC")
+		self.render("front.html", title=title, art=art, error=error, arts=arts)
 
 	def get(self):
 		self.render_front()
@@ -48,10 +54,13 @@ class MainPage(Handler):
 		art = self.request.get("art")	
 
 		if title and art:
-			self.write("thanks!")
+			a = Art(title = title, art = art)
+			a.put()
+
+			self.redirect("/")
 		else:
 			error = "we need both a title and artwork"
-			self.render("front.html", error = error)
+			self.render_front(title, art, error)
 
 app = webapp2.WSGIApplication([('/', MainPage)], debug=True)
 	#def render_front(self, title="", art="", errors=""):
